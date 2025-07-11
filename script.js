fbb9db22e1edf79717bf4eb6f23aa90ebb981220
halilogluyemekcilik <osmanhaliloglu54@gmail.com>
2025-07-11 12:10:38 +0300

    first code

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Hamburger Menü
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        // Toggle Nav
+        nav.classList.toggle('active');
+
+        // Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+
+        // Burger Animation
+        burger.classList.toggle('toggle');
+    });
+
+    // Otomatik Yıl Güncellemesi
+    const currentYearSpan = document.getElementById('currentYear');
+    if (currentYearSpan) {
+        currentYearSpan.textContent = new Date().getFullYear();
+    }
+});
